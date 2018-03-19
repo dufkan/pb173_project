dbed5357f1e0b0b3cfba0cc1cd051dd489eb7ebf
stepgenn <s.Gennertova@seznam.cz>
2018-03-19 10:35:07 +0100

    In crypto.cpp added pad and unpad

---
+++ shared/crypto.cpp
@@ -0,0 +1,18 @@
+#include "crypto.hpp"
+#include<vector>
+
+void cry::pad(std::vector<uint8_t>& data, uint8_t bsize){
+    int8_t val = bsize - (data.size() % bsize);
+    for(uint8_t i = 0; i < val; ++i)
+    data.push_back(val);
+}
+
+
+void cry::unpad(std::vector<uint8_t>& data, uint8_t bsize) {
+    if(data.size() < bsize) return;
+    uint8_t val = data[data.size() - 1];
+    if(val > bsize) return;
+    data.resize(data.size() - val);
+}
+
+
