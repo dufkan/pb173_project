43628fbc651e56e86b94e152c958bb1affed8ab8
stepgenn <s.Gennertova@seznam.cz>
2018-03-25 22:08:49 +0200

    crypto.hpp

--- shared/crypto.hpp
+++ shared/crypto.hpp
@@ -97,9 +97,27 @@ std::vector<uint8_t> decrypt_aes(const std::vector<uint8_t>& data, std::array<ui
 template <typename C>
 std::vector<uint8_t> encrypt_rsa(const C& data,  mbedtls_rsa_context* rsa_pub) {
     std::vector<uint8_t> result;
-    result.resize(data.size());
-    mbedtls_rsa_public(rsa_pub, data.data(), result.data());
+
+    if((mbedtls_rsa_complete(rsa_pub)!=0) || mbedtls_rsa_check_pubkey(rsa_pub)){
+        result.resize(1);
+        return result;
+    }
+    result.resize(512);
+
+    const char *pers = "rsa_decrypt";
+    mbedtls_entropy_context entropy;
+    mbedtls_ctr_drbg_context ctr_drbg;
+    mbedtls_ctr_drbg_init(&ctr_drbg);
+    mbedtls_entropy_init(&entropy);
+
+    mbedtls_ctr_drbg_seed( &ctr_drbg, mbedtls_entropy_func, &entropy, (const unsigned char *) pers,strlen(pers));
+
+
+    mbedtls_rsa_pkcs1_encrypt(rsa_pub, mbedtls_ctr_drbg_random, &ctr_drbg, MBEDTLS_RSA_PUBLIC, data.size(), data.data(), result.data());
+    mbedtls_ctr_drbg_free(&ctr_drbg);
+    mbedtls_entropy_free(&entropy);
     return result;
+
 }
 
 /**
@@ -113,16 +131,20 @@ std::vector<uint8_t> encrypt_rsa(const C& data,  mbedtls_rsa_context* rsa_pub) {
 std::vector<uint8_t> decrypt_rsa(const std::vector<uint8_t>& data, mbedtls_rsa_context* rsa_priv) {
     std::vector<uint8_t> result;
     result.resize(data.size());
-
+    if (mbedtls_rsa_complete(rsa_priv)!=0) {
+            result.resize(1);
+            return result;
+    }
+    result.resize(512);
     const char *pers = "rsa_decrypt";
     mbedtls_entropy_context entropy;
     mbedtls_ctr_drbg_context ctr_drbg;
     mbedtls_ctr_drbg_init(&ctr_drbg);
     mbedtls_entropy_init(&entropy);
-
+    size_t i = 512;
     mbedtls_ctr_drbg_seed( &ctr_drbg, mbedtls_entropy_func, &entropy, (const unsigned char *) pers,strlen(pers));
 
-    mbedtls_rsa_private( rsa_priv, mbedtls_ctr_drbg_random, &ctr_drbg, data.data(), result.data());
+    mbedtls_rsa_pkcs1_decrypt(rsa_priv, mbedtls_ctr_drbg_random, &ctr_drbg, MBEDTLS_RSA_PRIVATE, &i, data.data(), result.data(),1024);
 
     mbedtls_ctr_drbg_free( &ctr_drbg );
     mbedtls_entropy_free( &entropy );
@@ -157,22 +179,26 @@ bool check_hash(std::vector<uint8_t> data, std::array <uint8_t,32> control_hash)
 /** 
  * Generate random data of the length len
  *
- * @param data - block of memory of length len
  * @param len - length of the data
+ * @return - block of random data of length len
  */
-void get_random_data(uint8_t* data, size_t len) {
+std::vector<uint8_t> cry::get_random_data(size_t len) {
     mbedtls_ctr_drbg_context ctr_drbg;
     mbedtls_entropy_context entropy;
+    std::vector<uint8_t> result;
+    result.resize(len);
 
     const char *pers = "some random string";
 
     mbedtls_entropy_init(&entropy);
     mbedtls_ctr_drbg_init(&ctr_drbg);
     mbedtls_ctr_drbg_seed(&ctr_drbg, mbedtls_entropy_func, &entropy, (unsigned char *) pers, strlen(pers));
-    mbedtls_ctr_drbg_random( &ctr_drbg, data, len);
+    mbedtls_ctr_drbg_random( &ctr_drbg, result.data(), len);
     mbedtls_entropy_free(&entropy);
     mbedtls_ctr_drbg_free(&ctr_drbg);
+    return result;
 }
+
 
 /**
  * Create new pair od keys for RSA
@@ -180,45 +206,40 @@ void get_random_data(uint8_t* data, size_t len) {
 * @param prikey - the new private key will be saved here
  * @param pubkey - the new public key will be saved here
  */
-<<<<<<< HEAD
-void generate_rsa_keys(mbedtls_rsa_context* rsa_pub, mbedtls_rsa_context* rsa_priv);
-=======
-void generate_keys(mbedtls_rsa_context* rsa_pub, mbedtls_rsa_context* rsa_priv) {
+
+void generate_rsa_keys(mbedtls_rsa_context* rsa_pub, mbedtls_rsa_context* rsa_priv) {
     int exponent = 65537;
-    unsigned int key_size = 2048;
+    unsigned int key_size = 4096;
     mbedtls_rsa_context rsa;
     mbedtls_entropy_context entropy;
     mbedtls_ctr_drbg_context ctr_drbg;
-    mbedtls_mpi N, P, Q, D, E;/*, DP, DQ, QP;*/
+    mbedtls_mpi N, P, Q, D, E;
     const char *pers = "rsa_genkey";
 
-    //mbedtls_rsa_free(rsa_pub);mbedtls_rsa_free(rsa_priv);
-    //mbedtls_rsa_init(rsa_pub, MBEDTLS_RSA_PKCS_V21, MBEDTLS_MD_SHA256);
-    //mbedtls_rsa_init(rsa_priv, MBEDTLS_RSA_PKCS_V21, MBEDTLS_MD_SHA256);
-    
+    mbedtls_rsa_free(rsa_pub);mbedtls_rsa_free(rsa_priv);
+    mbedtls_rsa_init(rsa_pub, MBEDTLS_RSA_PKCS_V21, MBEDTLS_MD_SHA256);
+    mbedtls_rsa_init(rsa_priv, MBEDTLS_RSA_PKCS_V21, MBEDTLS_MD_SHA256);
+
     mbedtls_ctr_drbg_init( &ctr_drbg );
     mbedtls_rsa_init( &rsa, MBEDTLS_RSA_PKCS_V21, MBEDTLS_MD_SHA256);
     mbedtls_mpi_init( &N ); mbedtls_mpi_init( &P ); mbedtls_mpi_init( &Q );
-    mbedtls_mpi_init( &D ); mbedtls_mpi_init( &E );/* mbedtls_mpi_init( &DP );
-    mbedtls_mpi_init( &DQ ); mbedtls_mpi_init( &QP );*/
+    mbedtls_mpi_init( &D ); mbedtls_mpi_init( &E );
     mbedtls_entropy_init( &entropy );
     mbedtls_ctr_drbg_seed( &ctr_drbg, mbedtls_entropy_func, &entropy,(const unsigned char *) pers, strlen(pers));
 
     mbedtls_rsa_gen_key( &rsa, mbedtls_ctr_drbg_random, &ctr_drbg, key_size, exponent);
     mbedtls_rsa_export( &rsa, &N, &P, &Q, &D, &E );
-    /*mbedtls_rsa_export_crt( &rsa, &DP, &DQ, &QP)*/
-    
+
     mbedtls_rsa_import(rsa_pub, &N, NULL, NULL, NULL, &E);
     mbedtls_rsa_import(rsa_priv, &N, &P, &Q, &D, &E);
-    
+
     mbedtls_mpi_free( &N ); mbedtls_mpi_free( &P ); mbedtls_mpi_free( &Q );
-    mbedtls_mpi_free( &D ); mbedtls_mpi_free( &E ); /*mbedtls_mpi_free( &DP );
-    mbedtls_mpi_free( &DQ ); mbedtls_mpi_free( &QP );*/
+    mbedtls_mpi_free( &D ); mbedtls_mpi_free( &E );
     mbedtls_rsa_free( &rsa );
     mbedtls_ctr_drbg_free( &ctr_drbg );
     mbedtls_entropy_free( &entropy );
 }	
->>>>>>> d3152f78f26eee0fe6108c8993bcbeafd4408384
+
 
 /**
  * Create key by hashing data from fisrt_part and second_part
@@ -226,10 +247,15 @@ void generate_keys(mbedtls_rsa_context* rsa_pub, mbedtls_rsa_context* rsa_priv) 
  * @param first_part - data from challenge
  * @param second_part - data from response
  * @return symetric key created from chall and resp
- */ 
+ */
+/*
 std::array<uint8_t,32> create_symmetric_key(std::vector<uint8_t> first_part, std::vector<uint8_t> second_part);
+    first_part.insert(first_part.end(),second_part.begin(),second_part.end());
+    return cry::hash_sha(first_part);
 
-} // namespace cry
+} 
+*/
+// namespace cry
 
 
 #endif
