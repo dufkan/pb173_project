a77d69409aef574b5e775c193904debd82e85498
stepgenn <s.Gennertova@seznam.cz>
2018-04-02 23:47:12 +0200

    client handling with send and recv msg :email: :mailbox:

--- client/client.hpp
+++ client/client.hpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include <string>
 #include <iostream>
+#include <map>
 
 #include "asio.hpp"
 #include "../shared/messages.hpp"
@@ -16,8 +17,9 @@ class Client {
 #ifdef TESTMODE
 public:
 #endif
-    Channel chan;
     std::map<std::string, std::array<uint8_t,32>> contacts;
+
+     
 
     std::vector<uint8_t> client_challenge(std::array<uint8_t, 32> Rc, cry::RSAKey& rsa_pub, std::string pseudo, std::vector<uint8_t> key) {
         Encoder e;
@@ -69,20 +71,86 @@ public:
      *
      * @return message Send
      */
-    msg::Send create_message(std::string recv_name, std::vector<uint8_t> text) {
-	auto it = contacts.find(recv_name);
-	if (it == contacts.end()) {
-	    /* Send a message to server fot recv keys and prekeys.... */
-
-	}
-	std::array<uint8_t,32> recv_key = it->second;
-	std::vector text_enc = encrypt_aes(text, {}, recv_key);
+    msg::Send create_message(std::string recv_name, std::array<uint8_t,32> recv_key, std::vector<uint8_t> text) {
+	std::vector text_enc = cry::encrypt_aes(text, {}, recv_key);
         msg::Send msg_send(recv_name,text_enc);
-        return msg_send.move();
+        return msg_send;
     }
 
 
+
+    /**
+     * Send message text to client recv_name to channel
+     *
+     * @param recv_name Reciever name
+     * @param text Text of the message
+     */
+    void send_message(std::string recv_name, std::string text, Channel& chan){
+        std::vector<uint8_t> text_u(text.begin(), text.end());
+        auto it = contacts.find(recv_name);
+        if (it == contacts.end()) {
+            /* Send a message to server fot recv keys and prekeys.... */
+
+        }
+        std::array<uint8_t,32> recv_key = it->second; 	
+	msg::Send msg_send = create_message(recv_name, recv_key, text_u);
+	std::vector<uint8_t> byte_to_send = msg_send.serialize();
+	chan.send(byte_to_send);
+    }
+    
+    
+    /**
+     * Decrypt text of message in message Recv
+     * 
+     * @param msg_recv Recieved message
+     * @return Pair (sender_name, decrypted_text)
+     */
+
+    std::pair<std::string,std::vector<uint8_t>> decrypt_msg(msg::Recv& msg_recv) {
+	std::string sender_name = msg_recv.get_sender();
+	auto it = contacts.find(sender_name);
+        if (it == contacts.end()) {
+            /* Some error or resolution of it */
+
+        }
+        std::array<uint8_t,32> sender_key = it->second;
+	std::vector<uint8_t> text_dec = cry::decrypt_aes(msg_recv.get_text(),{},sender_key);
+	return std::make_pair(sender_name,text_dec);
+    }
+
+
+    /**
+     * From recived vytes get get the sender and the text of message
+     *
+     * @param msg_u Recieved bytes
+     * @return Pair(sender_name, text_of_message) 
+     */
+
+    std::pair<std::string,std::string> handle_recv_msg(std::vector<uint8_t> msg_u) {
+	std::unique_ptr<msg::Message> msg_des = msg::Recv::deserialize(msg_u);
+	msg::Recv& recv_des = dynamic_cast<msg::Recv&>(*msg_des.get());
+	auto sender_text = decrypt_msg(recv_des);
+	std::string text_s(reinterpret_cast<char*> (sender_text.second.data()),sender_text.second.size());
+	return std::make_pair(sender_text.first,text_s);
+    }
+
 public:
+
+
+    bool add_contact(std::string name, std::array<uint8_t,32> key) {
+        auto it = contacts.find(name);
+	if (it != contacts.end()) {
+		return false;
+	}
+	contacts[name]=key;
+	return true;
+    }
+    
+    std::array<uint8_t,32> get_key(std::string name) {
+	return contacts[name];
+    }
+
+
     void run() {
         using asio::ip::tcp;
         asio::io_service io_service;
